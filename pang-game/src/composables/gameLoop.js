69f198ba3da0b8fa5b91bec30865a3e3426b2d21
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2025-03-30 09:08:51 +0300

    fix game loop issue with speeding up the game after restart

--- pang-game/src/composables/gameLoop.js
+++ pang-game/src/composables/gameLoop.js
@@ -2,23 +2,25 @@ import { ref } from 'vue';
 
 export function useGameLoop() {
   const animationFrameId = ref(null);
-  
-  const startGameLoop = (updateFunction) => {
-    const animate = () => {
-      updateFunction();
-      animationFrameId.value = requestAnimationFrame(animate);
-    };
-    
-    animationFrameId.value = requestAnimationFrame(animate);
-  };
-  
+
   const stopGameLoop = () => {
-    if (animationFrameId.value) {
+    if (animationFrameId.value !== null) {
       cancelAnimationFrame(animationFrameId.value);
       animationFrameId.value = null;
     }
   };
-  
+
+  const startGameLoop = (updateFunction) => {
+    stopGameLoop();
+
+    const animate = () => {
+      updateFunction();
+      animationFrameId.value = requestAnimationFrame(animate);
+    };
+
+    animationFrameId.value = requestAnimationFrame(animate);
+  };
+
   return {
     startGameLoop,
     stopGameLoop
